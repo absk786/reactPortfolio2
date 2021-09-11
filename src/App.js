c6acb256ecee845c0feb53829f3202c00709562e
absk786 <rehmank11@gmail.com>
2021-09-11 10:22:20 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import './App.css';
 import Nav from "./Components/Nav/Nav";
 import Footer from "./Components/Footer/footer";
 import ContactForm from "./Components/Contact/index";
-import { Redirect } from "react-router";
+// import { Redirect } from "react-router";
 
 function App() {
   return (
     <BrowserRouter>
     <Nav/>
     <Switch >
-        <Route exact path='/' render = {()=> { return (<Redirect to='/'/>)}}/>
+        {/* <Route exact path='/' render = {()=> { return (<Redirect to='/'/>)}}/> */}
         <Route component={Home} path='/' exact/>
         <Route component={About} path='/about'/>
         <Route component={Projects} path='/projects'  />
