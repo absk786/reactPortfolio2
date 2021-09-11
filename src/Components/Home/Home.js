3ca95a14e866cdaf2864e3aaaa95c8b9e63f0489
absk786 <rehmank11@gmail.com>
2021-09-11 10:42:38 +0100

    updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,7 +4,7 @@ function Home () {
     return(
         <>
         <div id="aboutme" class="search">
-        <img src='../../../css/libs.jpeg' alt='asdfsadf'></img>   
+        <img src='../../../public/css/libs.jpeg' alt='asdfsadf'></img>   
         <h2 class="headertwo">Who Am!</h2>
         <p>Abdul is an astute, life long learner, and an extremely versatile individual with 5 successful of years of experience in agile digital products development. 
         </p>
