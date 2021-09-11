59e1a168e695000daa98307a62c633370017900b
absk786 <rehmank11@gmail.com>
2021-09-11 09:42:40 +0100

    path updated

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import './App.css';
 import Nav from "./Components/Nav/Nav";
 import Footer from "./Components/Footer/footer";
 import ContactForm from "./Components/Contact/index";
+import { Redirect } from "react-router";
 
 function App() {
   return (
     <BrowserRouter>
     <Nav/>
     <Switch >
+
+        <Route exact path='/' render = {()=> { return (<Redirect to='/'/>)}}/>
         <Route component={Home} path='/' exact/>
         <Route component={About} path='/about'  />
         <Route component={Projects} path='/projects'  />
